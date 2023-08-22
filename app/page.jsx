95af14467cc9f4994c3ee4a9e2523c6dd2624e07
stseshyts <istseshyts@gmail.com>
2023-08-22 15:01:30 +0300

    added some content to the page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>HOME</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text head_center">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI Powered Prompts</span>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 };
 
 export default Home;
