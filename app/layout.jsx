a4e791c6e59902310d1c5d4a0eef016daf07e04d
stseshyts <istseshyts@gmail.com>
2023-08-22 14:32:22 +0300

    added RootLayout and page into the app

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  descriptions: 'Discover and Share AI prompts',
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
